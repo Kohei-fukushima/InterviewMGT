2fd676a9608462fbd054e0c3099d00ce85c6dea3
KOHEI FUKUSHIMA <pecpec1129@gail.com>
2022-07-07 09:35:13 +0000

    devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+ config.hosts << "f92e24c5589e408a9f42e6d9c938acb8.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
