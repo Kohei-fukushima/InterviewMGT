f43e93d8d5fa93436deb555c4b5de7723abdc61d
KOHEI FUKUSHIMA <pecpec1129@gail.com>
2022-07-22 04:01:32 +0000

    マイグレーションファイル変更

--- db/migrate/20220707083733_devise_create_users.rb
+++ db/migrate/20220707083733_devise_create_users.rb
@@ -4,11 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :name, null: false, default: ""
-      t.string :email,              null: false, default: ""
+      t.string :name
+      t.string :email             , null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.text :introduction, null: false, default: ""
-      t.string :group, null: false, default: ""
+      t.text :introduction
+      t.string :group
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -100,11 +100,11 @@ ActiveRecord::Schema.define(version: 2022_07_18_055842) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", default: "", null: false
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.text "introduction", default: "", null: false
-    t.string "group", default: "", null: false
+    t.text "introduction"
+    t.string "group"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
